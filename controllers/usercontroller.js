e62187f117965b08f79a7339325fc30f23e84d55
viseth89 <viseth89@gmail.com>
2021-07-10 10:38:09 -0500

    Migrated paths reviewing changes

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,5 +1,11 @@
-const Express = require("express")
-const router = Express.Router()
+const router = require("express").Router();
+const { UserModel } = require("../models");
+const { UniqueConstraintError } = require("sequelize/lib/errors");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+
+
 
 router.get('/user', (req, res) => {
     res.send("This is the user controller")
@@ -8,5 +14,82 @@ router.get('/user', (req, res) => {
 router.get('/user1', (req, res) => {
     res.send("This is the user controller user1")
 })
+
+
+
+
+// Review
+router.post("/register", async (req, res) => {
+    let { email, password } = req.body.user;
+    try {
+      const User = await UserModel.create({
+        email,
+        password: bcrypt.hashSync(password, 13),
+      });
+  
+      let token = jwt.sign({ id: User.id, email: User.email }, "i_am_secret", {
+        expiresIn: 60 * 60 * 24,
+      });
+  
+      res.status(201).json({
+        message: "User successfully registered",
+        user: User,
+        sessionToken: token,
+      });
+    } catch (err) {
+      if (err instanceof UniqueConstraintError) {
+        res.status(409).json({
+          message: "Email already in use",
+        });
+      } else {
+        res.status(500).json({
+          message: "Failed to register user",
+        });
+      }
+    }
+  });
+  
+  router.post("/login", async (req, res) => {
+    let { email, password } = req.body.user;
+  
+    try {
+      const loginUser = await UserModel.findOne({
+        where: {
+          email: email,
+        },
+      });
+  
+      if (loginUser) {
+        let passwordComparison = await bcrypt.compare(
+          password,
+          loginUser.password
+        );
+  
+        if (passwordComparison) {
+          let token = jwt.sign({ id: loginUser.id }, process.env.JWT_SECRET, {
+            expiresIn: 60 * 60 * 24,
+          });
+  
+          res.status(200).json({
+            user: loginUser,
+            message: "User successfully logged in!",
+            sessionToken: token,
+          });
+        } else {
+          res.status(401).json({
+            message: "Incorrect email or password",
+          });
+        }
+      } else {
+        res.status(401).json({
+          message: "Login failed",
+        });
+      }
+    } catch (error) {
+      res.status(500).json({
+        message: "Failed to log user in",
+      });
+    }
+  });
 
 module.exports = router
