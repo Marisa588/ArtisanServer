8fd75393d222d9eb6b868784902e32c89704948a
Dean Howell <howelldean81@gmail.com>
2021-07-10 15:01:49 -0400

    pre-pull

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -15,19 +15,16 @@ router.get('/user1', (req, res) => {
     res.send("This is the user controller user1")
 })
 
-
-
-
-// Review
+Review
 router.post("/register", async (req, res) => {
-    let { email, password } = req.body.user;
+    let { username, password } = req.body.user;
     try {
       const User = await UserModel.create({
-        email,
+        username,
         password: bcrypt.hashSync(password, 13),
       });
   
-      let token = jwt.sign({ id: User.id, email: User.email }, "i_am_secret", {
+      let token = jwt.sign({ id: User.id, username: User.username }, "i_am_secret", {
         expiresIn: 60 * 60 * 24,
       });
   
@@ -49,47 +46,47 @@ router.post("/register", async (req, res) => {
     }
   });
   
-  router.post("/login", async (req, res) => {
-    let { email, password } = req.body.user;
-  
-    try {
-      const loginUser = await UserModel.findOne({
-        where: {
-          email: email,
-        },
-      });
-  
-      if (loginUser) {
-        let passwordComparison = await bcrypt.compare(
-          password,
-          loginUser.password
-        );
-  
-        if (passwordComparison) {
-          let token = jwt.sign({ id: loginUser.id }, process.env.JWT_SECRET, {
-            expiresIn: 60 * 60 * 24,
-          });
-  
-          res.status(200).json({
-            user: loginUser,
-            message: "User successfully logged in!",
-            sessionToken: token,
-          });
-        } else {
-          res.status(401).json({
-            message: "Incorrect email or password",
-          });
-        }
+router.post("/login", async (req, res) => {
+  let { email, password } = req.body.user;
+
+  try {
+    const loginUser = await UserModel.findOne({
+      where: {
+        email: email,
+      },
+    });
+
+    if (loginUser) {
+      let passwordComparison = await bcrypt.compare(
+        password,
+        loginUser.password
+      );
+
+      if (passwordComparison) {
+        let token = jwt.sign({ id: loginUser.id }, process.env.JWT_SECRET, {
+          expiresIn: 60 * 60 * 24,
+        });
+
+        res.status(200).json({
+          user: loginUser,
+          message: "User successfully logged in!",
+          sessionToken: token,
+        });
       } else {
         res.status(401).json({
-          message: "Login failed",
+          message: "Incorrect email or password",
         });
       }
-    } catch (error) {
-      res.status(500).json({
-        message: "Failed to log user in",
+    } else {
+      res.status(401).json({
+        message: "Login failed",
       });
     }
-  });
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed to log user in",
+    });
+  }
+});
 
 module.exports = router
