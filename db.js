8fd613252beeb257fa6dd8c7f7b5476a22bbe37e
Lauren Durr <laurenashdurr@gmail.com>
2021-07-10 11:07:35 -0400

    updated database to Rad_Records

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize("postgres://postgres:72hourwizards@vpostgres.csmzuruqvoj3.us-west-2.rds.amazonaws.com:5432/wizards")
+const sequelize = new Sequelize("postgres://postgres:72hourwizards@vpostgres.csmzuruqvoj3.us-west-2.rds.amazonaws.com:5432/Rad_Records")
 
 module.exports = sequelize
