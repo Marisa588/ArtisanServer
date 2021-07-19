61675a982e004603f3041cc9d51781c815b89229
Dean Howell <howelldean81@gmail.com>
2021-07-18 21:20:48 -0400

    Added .env stuff to protect the bucket. Oopsies they didn't make it to the last merge...

--- middleware/multer.js
+++ middleware/multer.js
@@ -4,28 +4,10 @@ const aws = require('aws-sdk')
 const multer = require("multer")
 const multerS3 = require('multer-s3')
 
-// aws.config.update({
-//     secretAccessKey: 'ruUZgwmiBDK0CDCjfyQkrB8ABtH55MSWiYlMM0od',
-//     accessKeyId: 'AKIAUN5P3JMEW5JLBRUL',
-//     region: 'us-west-2'
-// })
-
-// const s3 = new aws.S3({
-//     secretAccessKey: 'ruUZgwmiBDK0CDCjfyQkrB8ABtH55MSWiYlMM0od',
-//     accessKeyId: 'AKIAUN5P3JMEW5JLBRUL',
-//     bucket: 'eleven-wizards'
-// })
-
-// const s3 = new aws.S3({
-//     accessKeyId: 'AKIA23IMNJ77B2KKEK6L',
-//     secretAccessKey: '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',
-//     bucket: 'mrbearnewbucket'
-// });
-
 const s3 = new aws.S3({
-    accessKeyId: 'AKIAUN5P3JMEW5JLBRUL',
-    secretAccessKey: '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',
-    bucket: 'eleven-wizards'
+    accessKeyId: process.env.ACCESS_KEY,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+    bucket: process.env.BUCKET_NAME
 });
 
 const upload = multer({
@@ -42,9 +24,8 @@ const upload = multer({
         }
     })
 })
-
+// For local files
 // const upload = multer({ storage:fileStorageEngine })
-
-
 // module.exports = fileStorageEngine
+
 module.exports = upload
